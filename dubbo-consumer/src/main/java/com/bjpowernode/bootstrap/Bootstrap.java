a40bf7c2626906548612557cb9db05df839a6d37
gouyan <2637178209@qq.com>
2018-07-03 14:16:47 +0800

    gx

--- dubbo-consumer/src/main/java/com/bjpowernode/bootstrap/Bootstrap.java
+++ dubbo-consumer/src/main/java/com/bjpowernode/bootstrap/Bootstrap.java
@@ -7,7 +7,9 @@ import java.io.IOException;
 public class Bootstrap {
     public static void main(String[] args) throws IOException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+        /**获取远程provider的 helloService对象*/
         HelloService helloService = (HelloService) context.getBean("helloService");
+        /**通过动态代理，将对象，方法名称，参数发送到provider端执行，然后将返回值传递回来*/
         String result = helloService.sayHello("小唐");
         System.out.println(result);
     }
